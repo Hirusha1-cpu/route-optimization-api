b868c9d4819b3646a413aa137d8967e6f3564bf0
Hirusha1-cpu <hirushafernando121@gmail.com>
2026-09-09 22:49:07 +0530

    Updated

--- resources/js/api/client.js
+++ resources/js/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: 'https://route-optimization-api.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
