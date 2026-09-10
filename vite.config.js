045bc6ebb1d36d8aea2cd7c60717ec2e3d26b203
Hirusha1-cpu <hirushafernando121@gmail.com>
2026-09-10 07:10:29 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,14 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
-            refresh: true,
-        }),
         react({
             tsDecorators: false,
         }),
     ],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
     build: {
-        outDir: 'public/build',        // 👈 මේක add කරන්න!
+        outDir: 'dist',
         emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                app: 'resources/js/app.jsx',
-            },
-        },
     },
 });
